c1be8d09d88cf6a1f7854fd9d223220eb2002638
majabjjj <majabjjj@gmail.com>
2021-11-18 14:24:59 +0100

    27-save-reservation-in-db-table-reservations

--- modules/custom/movie_reservation/src/Controller/MovieReservation.php
+++ modules/custom/movie_reservation/src/Controller/MovieReservation.php
@@ -10,6 +10,7 @@ use Drupal\taxonomy\Entity\Term;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\paragraphs\Entity\Paragraph;
 use Drupal\Core\Database\Driver\mysql\Select;
+use Drupal\Core\Database\Connection;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -26,11 +27,14 @@ class MovieReservation extends ControllerBase{
       '#genres' => $this->getTaxonomy('genres'),
       '#days' => $this->getTaxonomy('days'),
       '#reservation' => $this->saveReservation(),
+      '#nid' => $this->getMovieId('nid'),
+
     ];
   }
 
+
   public function getMovieList(){
-   $selectedGenre= \Drupal::request()->query->get('selectedGenre');
+   $selectedGenre= \Drupal::request()->request->get('selectedGenre');
    if (!empty($selectedGenre)){
       $allNodeIds = \Drupal::entityQuery('node')->condition('type', 'movies')->condition('field_genres', strtolower($selectedGenre))->execute();
       }else{
@@ -44,14 +48,38 @@ class MovieReservation extends ControllerBase{
     return Term::loadMultiple($taxonomy);
   }
 
+  public function getMovieId($nid){
+    $movieid = \Drupal::entityQuery('node')->condition('nid', $nid)->execute();
+    return Node::loadMultiple($movieid);
+  }
+
+  public function queryForId(){
+
+  $movieid = db_query('SELECT movies FROM {node} WHERE nid = :nid', array(':nid' => $nid))->fetchField();
+  return $movieid;
+  }
+
   public function saveReservation(){
-    //$reservationData =  \Drupal::entityQuery('reservations')->insert();
-    $reservationData = \Drupal::request()->get('reservationData');
-    if (!empty($reservationData)){
-      $reservation =\Drupal::entityQuery('reservations')->insert();
-    }else{
-      $reservation = "Your reservation hasn't been recorded, please try again!";
-    }
+  $reservationData = \Drupal::request()->request->get('reservationData');
+  $connection = \Drupal\Core\Database\Database::getConnection();
+
+  if(!empty($reservationData)){
+    $result = $connection->insert('reservations')
+    ->fields([
+      'id' => $nid,
+      'day_of_reservation' => $reservationData[selectedDay],
+      'time_of_reservation' => ('Y-m-d h:i:s'),
+      'reserved_movie_name' => $movieid[selectedMovie],
+      'reserved_movie_genre' => $movieid[selectedMovie],
+      'customer_name' => $reservationData[name],
+
+    ])
+     ->execute();
+    return new JsonResponse ([ 'data' => 'Success! Your reservation has been saved!', 'method' => 'GET', 'status'=> 'success']);
+
+  }else{
+    return new JsonRepsone ([ 'data' => 'Error! Your reservation has not been saved!', 'method' => 'GET', 'status'=> 'error']);
+  }
   }
 
 }
