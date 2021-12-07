d344b7b319893685819e296d319da923d9e656f8
majabjjj <majabjjj@gmail.com>
2021-12-07 13:26:42 +0100

    30-31-fixing-empty-lines-and-changing-post-in-ajax-to-get

--- modules/custom/movie_reservation/src/Controller/MovieReservation.php
+++ modules/custom/movie_reservation/src/Controller/MovieReservation.php
@@ -1,21 +1,17 @@
 <?php
 
-
 namespace Drupal\movie_reservation\Controller;
 use Drupal\Code\Database\Database;
 use Drupal\node\Entity\Node;
 use Drupal;
 use Drupal\taxonomy\Entity\Term;
-
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\paragraphs\Entity\Paragraph;
 use Drupal\Core\Database\Driver\mysql\Select;
 use Drupal\Core\Database\Connection;
-
 use Symfony\Component\HttpFoundation\Request;
 use Drupal\Core\Ajax\AjaxResponse;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
 
 class MovieReservation extends ControllerBase{
 
@@ -33,7 +29,6 @@ class MovieReservation extends ControllerBase{
       '#reservation' => $this->saveReservation(),
     ];
   }
-
   /**
    * Function that provides page 'all-reservation' that has list of all reservations from database.
    * @return array
@@ -47,13 +42,13 @@ class MovieReservation extends ControllerBase{
   }
 
   public function getMovieList(){
-   $selectedGenre= \Drupal::request()->query->get('selectedGenre');
-   if (!empty($selectedGenre)){
-      $allNodeIds = \Drupal::entityQuery('node')->condition('type', 'movies')->condition('field_genres', strtolower($selectedGenre))->execute();
+    $selectedGenre= \Drupal::request()->query->get('selectedGenre');
+      if (!empty($selectedGenre)){
+        $allNodeIds = \Drupal::entityQuery('node')->condition('type', 'movies')->condition('field_genres', strtolower($selectedGenre))->execute();
       }else{
-            $allNodeIds = \Drupal::entityQuery('node')->condition('type', 'movies')->execute();
+        $allNodeIds = \Drupal::entityQuery('node')->condition('type', 'movies')->execute();
       }
-     return Node::loadMultiple($allNodeIds);
+    return Node::loadMultiple($allNodeIds);
   }
 
   public function getTaxonomy($var){
@@ -62,64 +57,47 @@ class MovieReservation extends ControllerBase{
   }
 
   public function saveReservation(){
+    $reservationData = \Drupal::request()->request->get('reservationData');
+    $data = json_decode($reservationData, true);
+    $connection = \Drupal\Core\Database\Database::getConnection();
+    $query = $connection->select('node_field_data');
+    $query->fields('node_field_data', ['vid', 'title']);
+    $query->condition('nid', $data["themovie"]);
+    $result = $query->execute();
+    $values = $result->fetchAll();  
 
-  $reservationData = \Drupal::request()->request->get('reservationData');
-  $data = json_decode($reservationData, true);
-  
-  $connection = \Drupal\Core\Database\Database::getConnection();
-  $query = $connection->select('node_field_data');
-  $query->fields('node_field_data', ['vid', 'title']);
-  $query->condition('nid', $data["themovie"]);
-  $result = $query->execute();
-  $values = $result->fetchAll();
-  
-  
-  
-  if(!empty($reservationData)){
-    $result = $connection->insert('reservations')
-    ->fields([
+    if(!empty($reservationData)){
+      $result = $connection->insert('reservations')
+      ->fields([
       'day_of_reservation' => $data["theday"],
       'time_of_reservation' => date('Y-m-d H:i:s'),
       'reserved_movie_name' => $values[0]->title,
       'reserved_movie_genre' => $values[0]->vid,
       'customer_name' => $data["thename"],
-    ])
-    ->execute();
-  
-    return new JsonResponse ([ 'data' => 'Success! Your reservation has been saved!', 'method' => 'GET', 'status'=> 'success']);
-
-    } else{
+      ])
+      ->execute();
+      return new JsonResponse ([ 'data' => 'Success! Your reservation has been saved!', 'method' => 'GET', 'status'=> 'success']);
+      } else {
         return new JsonResponse ([ 'data' => 'Error! Your reservation has not been saved!', 'method' => 'GET', 'status'=> 'error']);
-    }
+      }
   }
 
   public function getReservationList(){
-
     $selectedSort= \Drupal::request()->query->get('selectedSort');
     $database=  \Drupal\Core\Database\Database::getConnection();
 
     if($selectedSort == 'reserved_movie_name|ASC'){
-
       $query = $database->query('SELECT * FROM reservations ORDER BY reserved_movie_name ASC');
-
-    } elseif ($selectedSort == 'reserved_movie_name|DESC'){
-
-      $query= $database->query('SELECT * FROM reservations ORDER BY reserved_movie_name DESC');
-
-    } elseif ($selectedSort == 'id|DESC'){
-
-      $query= $database->query('SELECT * FROM reservations ORDER BY id DESC');
-
-    } else{
-      $query = $database->query('SELECT * FROM reservations');
-
-    }
-
+      } elseif ($selectedSort == 'reserved_movie_name|DESC'){
+          $query= $database->query('SELECT * FROM reservations ORDER BY reserved_movie_name DESC');
+            } elseif ($selectedSort == 'id|DESC'){
+                $query= $database->query('SELECT * FROM reservations ORDER BY id DESC');
+                  } else{
+                      $query = $database->query('SELECT * FROM reservations');
+                    }
     $reservations = $query->fetchAll();
     return $reservations;
-
   }
-
-  }
+ }
 
 
